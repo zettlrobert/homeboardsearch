9f95e8b71f1eeefe34fc0fdf1eae29d3108a1ef7
Unknown <zettl.robert@gmail.com>
2018-01-09 19:37:15 +0100

    got css Modules in Landingpage working

--- hbs-view/src/Landingpage/Landingpage.js
+++ hbs-view/src/Landingpage/Landingpage.js
@@ -4,7 +4,7 @@ import classes from './Landingpage.css';
 const LandingPage = () => {
   return (
     <div className={classes.Wrapper}>
-      <header>
+      <header className={classes.header}>
         <h1>Homeboard Search</h1>
       </header>
     </div>
